65ced983406ea40e02e4af14f111047ffed2a0a6
Imvisitor <99911138+Imvisitor@users.noreply.github.com>
2024-12-08 14:47:23 +0100

    Update luasnip.lua

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -1,34 +1,32 @@
 return {
-  'L3MON4D3/LuaSnip',   -- Pfad zum LuaSnip Repository
-  requires = 'rafamadriz/friendly-snippets',  -- Eine Sammlung von Snippets für viele Programmiersprachen
+  'L3MON4D3/LuaSnip',
+  requires = 'rafamadriz/friendly-snippets',
   config = function()
     local luasnip = require('luasnip')
-    
-    -- Standardkonfigurationen für LuaSnip
     luasnip.config.set_config({
-      history = true,             -- Ermöglicht das Navigieren durch Snippet-Vorgänger und -Nachfolger
-      updateevents = 'TextChanged,TextChangedI',  -- Update Snippets bei Textänderungen
+      history = true,
+      updateevents = 'TextChanged,TextChangedI',
     })
 
-    -- Lade die Snippets aus dem friendly-snippets Paket
     require("luasnip.loaders.from_vscode").lazy_load()
+    require("luasnip.loaders.from_vscode").lazy_load({ paths = { "./my-snippets" } })
 
-    -- Erweiterungen für mehr Funktionalität
-    require("luasnip.loaders.from_vscode").lazy_load({ paths = { "./my-snippets" } })  -- Pfad zu deinen persönlichen Snippets
-
-    -- Erstelle eigene Snippets (Beispiel)
     luasnip.snippets = {
-      lua = {  -- Snippets speziell für Lua
-        luasnip.parser.parse_snippet("fn", "function($1) $0 end"),  -- `fn` ist das Triggerwort
+      lua = {
+        luasnip.parser.parse_snippet("fn", "function($1) $0 end"),
       },
       python = {
-        luasnip.parser.parse_snippet("def", "def $1($2):\n    $0"),  -- Python-Funktionssnippet
+        luasnip.parser.parse_snippet("def", "def $1($2):\n    $0"),
+      },
+      c = {
+        luasnip.parser.parse_snippet("main", "#include <stdio.h>\n\nint main(int argc, char *argv[]) {\n    $0\n    return 0;\n}"),
+      },
+      java = {
+        luasnip.parser.parse_snippet("main", "public static void main(String[] args) {\n    $0\n}"),
       },
     }
-    
-    -- Optional: Tastenkombinationen für die Navigation durch Snippets (innerhalb eines Insert-Mode Mappings)
-    vim.api.nvim_set_keymap("i", "<Tab>", [[luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>']], { silent = true, expr = true })
-    vim.api.nvim_set_keymap("i", "<S-Tab>", [[luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>']], { silent = true, expr = true })
+
+    vim.api.nvim_set_keymap("i", "<Tab>",   [[luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>']],   { silent = true, expr = true })
+    vim.api.nvim_set_keymap("i", "<S-Tab>", [[luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>']],             { silent = true, expr = true })
   end
 }
-
